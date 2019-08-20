957754cb1dd0b378b55d6c48180da3d1ca1fcc9d
penghuaping <penghuaping@huobi.com>
2019-08-20 15:29:19 +0800

    替换日志文件包

--- src/test/java/com/huobi/client/examples/GetCandlestickData.java
+++ src/test/java/com/huobi/client/examples/GetCandlestickData.java
@@ -2,42 +2,41 @@ package com.huobi.client.examples;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.huobi.client.AsyncRequestClient;
 import com.huobi.client.SyncRequestClient;
 import com.huobi.client.model.Candlestick;
 import com.huobi.client.model.enums.CandlestickInterval;
 
-
+@Slf4j
 public class GetCandlestickData {
   public static void main(String[] args) {
     // Synchronization mode
     SyncRequestClient syncRequestClient = SyncRequestClient.create();
     List<Candlestick> candlestickList = syncRequestClient.getLatestCandlestick(
         "btcusdt", CandlestickInterval.MIN1, 1);
-    System.out.println("---- 1 min candlestick for btcusdt ----");
+    log.info("---- 1 min candlestick for btcusdt ----");
     for (Candlestick candlestick : candlestickList) {
-      System.out.println();
-      System.out.println("Timestamp: " + candlestick.getTimestamp());
-      System.out.println("High: " + candlestick.getHigh());
-      System.out.println("Low: " + candlestick.getLow());
-      System.out.println("Open: " + candlestick.getOpen());
-      System.out.println("Close: " + candlestick.getClose());
-      System.out.println("Volume: " + candlestick.getVolume());
+      log.info("Timestamp: " + candlestick.getTimestamp());
+      log.info("High: " + candlestick.getHigh());
+      log.info("Low: " + candlestick.getLow());
+      log.info("Open: " + candlestick.getOpen());
+      log.info("Close: " + candlestick.getClose());
+      log.info("Volume: " + candlestick.getVolume());
     }
-    System.out.println();
     // Asynchronization mode
     AsyncRequestClient asyncRequestClient = AsyncRequestClient.create();
     asyncRequestClient.getLatestCandlestick("btcusdt", CandlestickInterval.MIN1, 1, (candlestickResult) -> {
       if (candlestickResult.succeeded()) {
-        System.out.println("---- 1 min candlestick for btcusdt ----");
+        log.info("---- 1 min candlestick for btcusdt ----");
         for (Candlestick candlestick : candlestickResult.getData()) {
-          System.out.println();
-          System.out.println("Timestamp: " + candlestick.getTimestamp());
-          System.out.println("High: " + candlestick.getHigh());
-          System.out.println("Low: " + candlestick.getLow());
-          System.out.println("Open: " + candlestick.getOpen());
-          System.out.println("Close: " + candlestick.getClose());
-          System.out.println("Volume: " + candlestick.getVolume());
+          log.info("Timestamp: " + candlestick.getTimestamp());
+          log.info("High: " + candlestick.getHigh());
+          log.info("Low: " + candlestick.getLow());
+          log.info("Open: " + candlestick.getOpen());
+          log.info("Close: " + candlestick.getClose());
+          log.info("Volume: " + candlestick.getVolume());
         }
       }
     });
